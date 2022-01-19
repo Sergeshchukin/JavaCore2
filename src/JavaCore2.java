49729d6d5f382103dbcebcdef7f6e4c0aff53d86
Sergey <94902372+Sergeshchukin@users.noreply.github.com>
2022-01-19 20:24:38 +0300

    Update JavaCore2.java

--- src/JavaCore2.java
+++ src/JavaCore2.java
@@ -27,6 +27,27 @@ public class JavaCore2 {
             System.out.println(m.getMessage());
         }
 
+        
+        public static int array(String[][] stringMatrix) throws MyArrayDataException, MyArraySizeException {
+        if (stringMatrix.length != 4 || stringMatrix[0].length != 4) {
+            throw new MyArraySizeException("ошибка длины массива");
+        }
+
+        int sum = 0;
+          for (int i = 0; i < stringMatrix.length; i++) {
+              for (int j = 0; j < stringMatrix.length; j++){
+                  try {
+                      sum += Integer.parseInt(stringMatrix[i][j]);
+                  } catch (NumberFormatException e) {
+                      throw new MyArrayDataException(" Ошибка в строке " +(i+1) +" в столбце " +(j+1));
+                  }
+             }
+          }
+          return sum;
+      }
+    }
+
+
 
     }
 }
